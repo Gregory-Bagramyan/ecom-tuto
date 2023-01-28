61a44b566223554a20f9ce07611a1e9897119414
Gregory <85247790+Gregory-Bagramyan@users.noreply.github.com>
2023-01-28 17:19:57 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div calssName="categories-container">
+      <div calssName="category-container">
+        {/* <img/> */}
+        <div calssName="category-body-container"></div>
+        <h2>Hats</h2>
+        <p>Shop Now</p>
+        <div></div>
+      </div>
+      <div calssName="category-container">
+        {/* <img/> */}
+        <div calssName="category-body-container"></div>
+        <h2>Jackets</h2>
+        <p>Shop Now</p>
+        <div></div>
+      </div>
+      <div calssName="category-container">
+        {/* <img/> */}
+        <div calssName="category-body-container"></div>
+        <h2>Sneakers</h2>
+        <p>Shop Now</p>
+        <div></div>
+      </div>
+      <div calssName="category-container">
+        {/* <img/> */}
+        <div calssName="category-body-container"></div>
+        <h2>Wemens</h2>
+        <p>Shop Now</p>
+        <div></div>
+      </div>
+      <div calssName="category-container">
+        {/* <img/> */}
+        <div calssName="category-body-container"></div>
+        <h2>Men</h2>
+        <p>Shop Now</p>
+        <div></div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
