f88dcad9b012fd2861f061dc05407187f0a84331
Gregory <85247790+Gregory-Bagramyan@users.noreply.github.com>
2023-01-28 21:04:05 +0100

    split display into components

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
+import Directory from "./components/directory/directory.component.jsx";
+
 const App = () => {
-  return (
-    <div calssName="categories-container">
-      <div calssName="category-container">
-        {/* <img/> */}
-        <div calssName="category-body-container"></div>
-        <h2>Hats</h2>
-        <p>Shop Now</p>
-        <div></div>
-      </div>
-      <div calssName="category-container">
-        {/* <img/> */}
-        <div calssName="category-body-container"></div>
-        <h2>Jackets</h2>
-        <p>Shop Now</p>
-        <div></div>
-      </div>
-      <div calssName="category-container">
-        {/* <img/> */}
-        <div calssName="category-body-container"></div>
-        <h2>Sneakers</h2>
-        <p>Shop Now</p>
-        <div></div>
-      </div>
-      <div calssName="category-container">
-        {/* <img/> */}
-        <div calssName="category-body-container"></div>
-        <h2>Wemens</h2>
-        <p>Shop Now</p>
-        <div></div>
-      </div>
-      <div calssName="category-container">
-        {/* <img/> */}
-        <div calssName="category-body-container"></div>
-        <h2>Men</h2>
-        <p>Shop Now</p>
-        <div></div>
-      </div>
-    </div>
-  );
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return <Directory categories={categories} />;
 };
 
 export default App;
